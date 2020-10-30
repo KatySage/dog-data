aa73529648fdc8dc23098d07c730be016cc1cb46
Katy Sage <katysagehj@gmail.com>
2020-10-30 10:14:56 -0400

    req

--- react-backend/dog/src/components/Demo.jsx
+++ react-backend/dog/src/components/Demo.jsx
@@ -187,7 +187,7 @@ export default class Demo extends React.PureComponent {
     //     addedAppointment.endDate = (moment(addedAppointment.startDate).format(format))
     this.setState({ addedAppointment });
   }
-<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
   changeAppointmentChanges(appointmentChanges) {
     this.setState({ appointmentChanges });
   }
